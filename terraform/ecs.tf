d2684a00b0d8d038590773b51769763c2d052e79
Craig Fox <craig.fox@icloud.com>
2026-09-02 13:56:56 +1200

    Set up base infrastructure for ECS

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -76,3 +76,31 @@ resource "aws_cloudwatch_log_group" "ecs" {
     Name = "${local.resource_prefix}-ecs-logs"
   }
 }
+
+# ECS security group
+resource "aws_security_group" "ecs" {
+  name        = "${local.resource_prefix}-ecs-sg"
+  description = "Security group for ECS tasks."
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "${local.resource_prefix}-ecs-sg"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "ecs_all" {
+  security_group_id = aws_security_group.ecs.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "rds_postgres_from_ecs" {
+  security_group_id            = aws_security_group.rds.id
+  referenced_security_group_id = aws_security_group.ecs.id
+
+  from_port   = 5432
+  to_port     = 5432
+  ip_protocol = "tcp"
+}
+
